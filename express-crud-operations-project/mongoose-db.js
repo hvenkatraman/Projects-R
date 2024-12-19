fb310bccc447be36e54bc2274f45c36e010e2c8d
hvenkatraman <hvenkatraman9@gmail.com>
2024-12-19 09:35:12 +0530

    Changes to be committed: 	modified:   express-crud-operations-project/mongoose-db.js
     Untracked files:
    	express-crud-operations-project/Express-Test.apiclient.json

--- express-crud-operations-project/mongoose-db.js
+++ express-crud-operations-project/mongoose-db.js
@@ -1,11 +1,12 @@
 const express=require("express");
 const mongoose=require("mongoose");
 
+
 const app=express();
 
 app.use(express.json());
 
-connectionurl="mongodb://localhost:27017/schoolDb";
+connectionurl="mongodb://localhost:27017/SchoolDb";
 mongoose.connect(connectionurl)
     .then(()=>{console.log("Mongoose database connection established sucessfully")})
     .catch((error)=>{console.log(error)});
@@ -26,16 +27,34 @@ app.post("/student/single",async(req,res,next)=>{
     const {Name,Email,Age,Department}=req.body;
 
     const newStudent=new Student({
-        Name:Name,
-        Email:Email,
-        Age:Age,
-        Department:Department
-    });
+
+    "Name": "Venkat12",
+    "Email": "hvenkatraman9a@gmail.com",
+    "Age": "38",
+    "Department": "Software Engineer12"
+})
+
+
         await newStudent.save()
 
     res.send("Student Database sucessfully created");
     }
     catch(error){res.status(500).json({message : error.message})}
+
+})
+app.post("/student/multiple",async(req,res,next)=>{
+
+    try{
+        const result=await Student.insertMany(req.body);
+        res.status(201).json({
+            "message":"Multiple documents added",
+            count:result.length
+        });
+
+
+    }catch(error){res.status(501).send(error.stack);}
+
+
 
 })
 app.listen(8000,()=>{console.log("Server running at localhost:8000")});
